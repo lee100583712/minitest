8dac42727c8cc570359f0fad34b957e51652f4e6
s21700497 <s21700497@walab.handong.edu>
2022-04-25 20:01:03 +0900

    load

--- manager.c
+++ manager.c
@@ -50,16 +50,29 @@ void saveData(Product p[], int count){
         printf("저장됨!\n");
 }
 
+
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
-	int count=0;
-	FILE*fp;
+        int count=0,i=0;
+        FILE*fp;
 
-	//파일 내용을 읽어와서 배열에 값 추가하기
+        //파일 내용을 읽어와서 배열에 값 추가하기
+
+        fp = fopen("product.txt", "rt");
+         if(fp == NULL){
+                 printf("=> 파일없음");
+         return 0;
+         }
+         for(; ; i++, count++){
+                 fscanf(fp, "%s", p[i].name);
+        if(feof(fp)) break;
+        fscanf(fp, "%d", &p[i].weight);
+        fscanf(fp, "%d", &p[i].price);
+    }
+    fclose(fp);
 
 
-
-
-	printf("=> 로딩 성공!\n");
-	return count;
+        printf("=> 로딩 성공!\n");
+        return count;
 }
+
